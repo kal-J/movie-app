fee27a22513c0ea62c2c29de8978f3b1b81df1d1
kal-J <kharljay15@gmail.com>
2021-01-07 14:37:46 +0300

    fix movie update

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -8,22 +8,29 @@ const appReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'ADD_MOVIE': {
       let newMovie = action.payload;
-      const movies = [...state.movies];
+      let movies = [...state.movies];
 
       // check if movie name exists and replace movie with new values
-      movies.some((movie, index) => {
+      let movieIndex;
+      movies.forEach((movie, index) => {
         if (movie.name === newMovie.name) {
-          newMovie = {
-            ...movies[index],
-            ...newMovie,
-          };
-
-          movies.slice(index, 1);
-
-          return true;
+          movieIndex = index;
         }
       });
 
+      // replace movie with same name as new movie
+      if (movieIndex !== undefined) {
+        movies[movieIndex] = {
+          ...movies[movieIndex],
+          ...newMovie,
+        };
+        const newState = {
+          movies,
+        };
+
+        return newState;
+      }
+      
       movies.push(newMovie);
 
       const newState = {
